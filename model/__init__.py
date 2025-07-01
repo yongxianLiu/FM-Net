086ccad74b9c68bbf965de2fa57e9ae3416763c6
yongxianLiu <125457815+yongxianLiu@users.noreply.github.com>
2025-07-01 21:32:47 +0800

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,21 @@
+from model.Unet.model_Unet import U_Net as Unet
+from model.ACM.model_ALCnet import ASKCResNetFPN as ALCNet
+from model.ACM.model_ACM import ASKCResUNet as ACM
+from model.DNANet.model_DNANet import DNANet as DNANet
+
+from model.RISTDnet.model_RISTDnet import RISTDnet as RISTDnet
+from model.UIUNet.model_UIUNet import UIUNET as UIUNet
+from model.ISTDUNet.model_ISTDUNet import ISTDU_Net as ISTDU_Net
+from model.RDIAN.model_RDIAN import RDIAN as RDIAN
+from model.MTUNet.MTU_Net import res_UNet as MTUNet
+from model.ResUNet.model_ResUNet import ResUNet as ResUNet
+
+from model.ResUNet.ResUNet_RuCB import ResUNet_RuCB as ResUNet_RuCB
+
+from model.WTAPNet.unet_model import UNet_F as WTAPNet
+
+from model.SCTransNet.SCTransNet import SCTransNet
+
+from model.SCTransNet.Config import get_SCTrans_config
+
+from model.SCTransNet.FMNet import FMNet
